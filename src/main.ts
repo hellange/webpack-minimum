e29f17d613dce17b04ab0cca0f2184395275ccdd
Helge Langehaug <hellange@cisco.com>
2017-11-08 12:16:59 +0100

    Added a typescript dependency.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { person } from "./person";
 
 var hello = document.getElementById("hello");
-hello.innerHTML = _.join(['Hello', 'webpack'], ' ');
+hello.innerHTML = _.join(['Hello, ', person.firstName, person.lastName], ' ');
